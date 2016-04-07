6d8590ef6abfc7b9a46157dab5c3b8349fdd24d4
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 17:05:13 -0700

    updated calculator to account for number of tokens

--- calculator.py
+++ calculator.py
@@ -17,25 +17,28 @@ def calculator(): #initialize loop that will execute given block of code
         if user_input == "q": # if q, quit (return none)
             return None
         tokens = user_input.split(" ") #tokenize input based on (" ")
-        tokens[1] = int(tokens[1]) #convert strings to integers
-        tokens[2] = int(tokens[2])
-         
+        tokens[1] = float(tokens[1]) #convert strings to integers
         
-        if tokens[0]== "+": #if/elif/else arithmetic signs
-            print add(tokens[1], tokens[2])
-        elif tokens[0] == "-":
-            print subtract(tokens[1], tokens[2])
-        elif tokens[0] == "*":
-            print multiply(tokens[1], tokens[2])
-        elif tokens[0] == "/":
-            print divide(tokens[1],tokens[2])
-        elif tokens[0] == "square":
-            print square(tokens[1],tokens[2])
-        elif tokens[0] == "cube":
-            print cube(tokens[1],tokens[2])
-        elif tokens[0] == "pow":
-            print power(tokens[1],tokens[2])
-        else:
-            print mod(tokens[1], tokens[2])
+        if len(tokens) > 2:
+            tokens[2] = float(tokens[2])
+            if tokens[0]== "+": #if/elif/else arithmetic signs
+                print add(tokens[1], tokens[2])
+            elif tokens[0] == "-":
+                print subtract(tokens[1], tokens[2])
+            elif tokens[0] == "*":
+                print multiply(tokens[1], tokens[2])
+            elif tokens[0] == "/":
+                print divide(tokens[1],tokens[2])
+            elif tokens[0] == "pow":
+                print power(tokens[1],tokens[2])
+            else:
+                print mod(tokens[1], tokens[2])
+        
+        elif len(tokens) <= 2:
+            if tokens[0] == "square":
+                print square(tokens[1])
+            elif tokens[0] == "cube":
+                print cube(tokens[1])
+        
 
 calculator()
