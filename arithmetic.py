665b77b707694c09e58105f478558397cee21c6e
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 18:16:27 -0700

    Step 2 further study, broken

--- arithmetic.py
+++ arithmetic.py
@@ -1,34 +1,37 @@
-def add(num1, num2):
-    return num1 + num2
+def add(*args):
+    result = 0
+    for arg in args:
+        result = result + arg 
+    return result 
 
+def subtract(*args):
+    for arg in args:
+        result = arg[0] - add(arg[1:])
+    return result
 
-def subtract(num1, num2):
-    return num1 - num2
-
-
-def multiply(num1, num2):
+def multiply(*args):
     return num1 * num2
 
 
-def divide(num1, num2):
+def divide(*args):
     # Need to turn at least argument to float for division to
     # not be integer division
     return float(num1) / float(num2) 
 
 
-def square(num1):
+def square(*args):
     # Needs only one argument
     return num1 * num1
 
 
-def cube(num1):
+def cube(*args):
     # Needs only one argument
     return num1 * num1 * num1
 
 
-def power(num1, num2):
+def power(*args):
     return num1 ** num2  # ** = exponent operator
 
 
-def mod(num1, num2):
+def mod(*args):
     return num1 % num2
