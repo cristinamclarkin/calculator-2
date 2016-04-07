c36cf83bb6790459be74ab66a7ca254f8cf04ad8
Hackbright Student <info@hackbrightacademy.com>
2016-04-07 12:43:37 -0700

    arithmetic.py modified to accept any number of variables

--- arithmetic.py
+++ arithmetic.py
@@ -1,37 +1,50 @@
-def add(*args):
+def add(*args): 
     result = 0
     for arg in args:
         result = result + arg 
     return result 
 
 def subtract(*args):
-    for arg in args:
-        result = arg[0] - add(arg[1:])
+    result = args[0]
+    for arg in args[1:]:
+        result = result - arg
     return result
 
 def multiply(*args):
-    return num1 * num2
+    result = 1
+    for arg in args:
+        result = result * arg
+    return result
 
 
 def divide(*args):
     # Need to turn at least argument to float for division to
     # not be integer division
-    return float(num1) / float(num2) 
-
+    result = float(args[0])
+    for arg in args[1:]:
+        result = result / arg
+    return result
 
 def square(*args):
     # Needs only one argument
-    return num1 * num1
-
+    for arg in args:
+        result = arg * arg
+    return result
 
 def cube(*args):
     # Needs only one argument
-    return num1 * num1 * num1
-
+    for arg in args:
+        result = arg * arg * arg
+    return result
 
 def power(*args):
-    return num1 ** num2  # ** = exponent operator
-
+    result = args[0]
+    for arg in args[1:]:
+        result = result ** arg
+    return result
 
 def mod(*args):
-    return num1 % num2
+    result = args[0]
+    for arg in args[1:]:
+        result = result % arg
+    return result
